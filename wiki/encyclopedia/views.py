8e3d8d3fba0b2714671565b7e2283de9cd9e27e1
Crisiwii <88282065+Auntjoestar@users.noreply.github.com>
2024-03-25 09:52:49 -0600

    Add access to entries in url and index

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from markdown2 import Markdown
 from . import util
 
 
@@ -7,4 +7,19 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def entries(request, title):
+    utilAnswer = util.get_entry(title)
+    if utilAnswer is not None:
+        return render(request, "encyclopedia/entries.html",{
+            "content": Markdown().convert(util.get_entry(title)),
+            "title" : title
+            })
+    else:
+        return render(request, "encyclopedia/error.html",{
+            "content": "Error 404: Page Not Found",
+            "title" : "Error 404"
+            })
+        
+    
 
